486b5a61c1b00440460cce416520520ddd122021
Iegor-Funtusov <funtushan@gmail.com>
2021-10-12 15:43:35 +0300

    add review hw 1

--- hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
@@ -3,7 +3,7 @@ package ua.com.alevel;
 public class BaseOperationsMain {
 
     public static void main(String[] args) {
-       
+        System.out.println("BaseOperationsMain.main");
         ProgramRun.run();
     }
 }
