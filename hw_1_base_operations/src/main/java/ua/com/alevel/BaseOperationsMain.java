a049ca88187958380520b8f92a22105c85fb8221
Iegor-Funtusov <funtushan@gmail.com>
2021-10-12 17:30:56 +0300

    final commit

--- hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
@@ -2,9 +2,7 @@ package ua.com.alevel;
 
 public class BaseOperationsMain {
 
-    public static void main(String[] args)
-    {
-        System.out.println("BaseOperationsMain.main");
+    public static void main(String[] args) {
         System.out.println("BaseOperationsMain.main");
         ProgramRun.run();
     }
