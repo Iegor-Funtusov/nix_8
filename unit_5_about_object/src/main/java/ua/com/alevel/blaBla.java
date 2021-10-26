bba3b2e895e65b9cf3cef67ed768f3aca691a577
Iegor-Funtusov <funtushan@gmail.com>
2021-10-26 17:17:13 +0300

    add ahtung!!! object overview

---
+++ unit_5_about_object/src/main/java/ua/com/alevel/blaBla.java
@@ -0,0 +1,28 @@
+package ua.com.alevel;
+
+//pozor!!!
+public class blaBla {
+
+    public static final int i = 10;
+
+    public void test1() {
+    }
+
+    public void test2() {
+    }
+
+
+    public void test3() {
+    }
+
+
+    public void test4() {
+
+        int a = 10;
+
+
+    }
+}
+
+
+
