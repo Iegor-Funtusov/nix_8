eeb04988f4f7724866bf60c06886e1bbdc7a7ba0
egorfuntusov <funtushan@gmail.com>
2022-02-17 18:10:16 +0200

    add testing

--- final_project/src/test/java/ua/com/alevel/final_project/web/OpenBookControllerSSRTest.java
+++ final_project/src/test/java/ua/com/alevel/final_project/web/OpenBookControllerSSRTest.java
@@ -3,20 +3,24 @@ package ua.com.alevel.final_project.web;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import ua.com.alevel.final_project.facade.PLPFacade;
 import ua.com.alevel.final_project.web.dto.response.BookPLPDto;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -33,14 +37,13 @@ public class OpenBookControllerSSRTest {
 
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private PLPFacade plpFacade;
 
     @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
-//                .apply(springSecurity())
                 .build();
     }
 
@@ -73,12 +76,14 @@ public class OpenBookControllerSSRTest {
 
     @Test
     public void shouldDoReturnViewPageWhenParamIsNotValid() throws Exception {
+        final List<BookPLPDto> list = new ArrayList<>();
+        when(plpFacade.search(Mockito.any())).thenReturn(list);
+
         MockHttpServletRequestBuilder createMessage = get("/books");
         this.mockMvc.
                 perform(createMessage)
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("/pages/open/plp"))
-                .andExpect(model().attribute("bookList", new ArrayList<BookPLPDto>()))
-        ;
+                .andExpect(model().attribute("bookList", list));
     }
 }
